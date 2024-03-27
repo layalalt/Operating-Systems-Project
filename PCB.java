36870927d86d23f9a9b7304f36a1956c8d30018a
layalalt <147754472+layalalt@users.noreply.github.com>
2024-03-27 06:25:50 +0300

    PCB.java

--- PCB.java
+++ PCB.java
@@ -1,4 +1,37 @@
-public class PCB
-{
 
+public class PCB implements Comparable<PCB>
+{
+    
+    String processID;
+    int priority;
+    int arrivalTime;
+    int cpuBurst;
+    int startTime;
+    int terminationTime;
+    int turnaroundTime;
+    int waitingTime;
+    int responseTime;
+    int remainingTime;
+    int timeSliceRemainingTime;
+
+    PCB(int ID, int priority, int arrivalTime, int cpuBurst) 
+    {
+        this.processID = "P" + ID;
+        this.priority = priority;
+        this.arrivalTime = arrivalTime;
+        this.cpuBurst = cpuBurst;
+        responseTime = -1;
+        timeSliceRemainingTime = -1;
+        remainingTime = cpuBurst;
+    }
+
+    @Override
+    public String toString() {
+        return "PCB{" + "processID=" + processID + ", priority=" + priority + ", arrivalTime=" + arrivalTime + ", cpuBurst=" + cpuBurst + ", startTime=" + startTime + ", terminationTime=" + terminationTime + ", turnaroundTime=" + turnaroundTime + ", waitingTime=" + waitingTime + ", responseTime=" + responseTime + '}';
+    }
+    
+    public int compareTo(PCB p) 
+    {
+        return this.cpuBurst - p.cpuBurst;
+    }
 }
